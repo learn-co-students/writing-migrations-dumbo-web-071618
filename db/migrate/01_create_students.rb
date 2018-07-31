5a69e836cf5d5c366161bb342453d41cb875131a
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-07-31 16:38:54 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+# We've created a class for you called CreateStudents.
+class CreateStudents < ActiveRecord::Migration[5.1] # To get access to the
+# ActiveRecord methods, don't forget to inherit from ActiveRecord::Migration.
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
